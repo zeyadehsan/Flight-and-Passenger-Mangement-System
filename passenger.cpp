3ec50ad04f8b9fdaf08928ad6c3e36028e7d46b9
Mo'men Osman <145454820+moamenosman@users.noreply.github.com>
2024-11-10 21:42:04 +0200

    Update passenger.cpp

--- passenger.cpp
+++ passenger.cpp
@@ -2,3 +2,29 @@
 #include "passenger.h"
 using namespace std;
 
+int Passenger::totalPassengers = 0;
+//Two argument constructor  
+Passenger::Passenger(string name_of_p,int id):NameOfPassenger(name_of_p),passenger_id(id){
+  totalPassengers++;
+}
+//Destructor
+Passenger::~Passenger(){
+  totalPassengers--;
+  cout<< "Passenger : " << NameOfPassenger << " has been removed."<<endl;
+}
+//Func
+void passenger::displayPassengerDetalils(){
+cout<< "Passenger's name : " << NameOfPassenger<<endl;
+cout<< "Passenger ID : " <<passenger_id<<endl;
+}
+istream& operator>>(istream & in ,const Passenger& p){
+cout<< "Enter Your name : "
+in>> p.NameOfPassenger;
+cout<<endl;
+cout<< "Enter Your ID : "
+in>> p.passenger_id;
+return in;
+}
+int Passenger::getTotalPassengers(){
+return totalPassengers
+}
